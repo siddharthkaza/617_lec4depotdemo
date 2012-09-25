b930181ac6bcf02cfdef9ca1c936d91117dd6a5b
skaza <skaza@towson.edu>
2012-09-25 17:46:13 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lec4Depotdemo32::Application.config.secret_token = 'a2fc46fb7b06b79483dcba36ca454becba51bdc3804d445bff0f7de159a94b093fa44d7235f4a18306f31c1dc14a16b79b3fbe9db517f7121bf9d0ba5639c322'
